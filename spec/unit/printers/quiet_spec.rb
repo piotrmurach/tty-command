6092ff548d8f20d1d4b6c004ec71e17c9e83d48b
Adam Doppelt <adoppelt@homeaway.com>
2016-05-15 16:09:43 -0500

    Added quiet printer that only prints output, not start & end. Closes

---
+++ spec/unit/printers/quiet_spec.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+RSpec.describe TTY::Command::Printers::Quiet do
+  let(:output) { StringIO.new }
+
+  it "doesn't print command start or exit" do
+    printer = TTY::Command::Printers::Quiet.new(output)
+    cmd = TTY::Command::Cmd.new("echo hello")
+
+    printer.print_command_start(cmd)
+    printer.print_command_exit(cmd, 0)
+    output.rewind
+
+    expect(output.string).to be_empty
+  end
+
+  it "prints command stdout data" do
+    printer = TTY::Command::Printers::Quiet.new(output)
+    cmd = TTY::Command::Cmd.new("echo hello")
+
+    printer.print_command_out_data(cmd, 'hello', 'world')
+    output.rewind
+
+    expect(output.string).to eq("helloworld")
+  end
+
+  it "prints command stderr data" do
+    printer = TTY::Command::Printers::Quiet.new(output)
+    cmd = TTY::Command::Cmd.new("echo hello")
+
+    printer.print_command_err_data(cmd, 'hello', 'world')
+    output.rewind
+
+    expect(output.string).to eq("helloworld")
+  end
+
+end
