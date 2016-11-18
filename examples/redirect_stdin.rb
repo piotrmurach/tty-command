118f6df3a281ae5d9a91df922d0f611c903ee876
Piotr Murach <pmurach@gmail.com>
2016-11-18 22:21:00 +0000

    Change wording.

--- examples/redirect_stdin.rb
+++ examples/redirect_stdin.rb
@@ -5,8 +5,8 @@ require 'tty-command'
 cmd = TTY::Command.new
 
 stdin = StringIO.new
-stdin.puts "dupa"
-stdin.puts "wolowa"
+stdin.puts "hello"
+stdin.puts "world"
 stdin.rewind
 
 out, _ = cmd.run(:cat, :in => stdin)
