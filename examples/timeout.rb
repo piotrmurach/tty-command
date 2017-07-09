10b6dedad18ad39805a4ac6395171702a3f6f50f
Piotr Murach <pmurach@gmail.com>
2017-07-09 20:39:06 +0100

    Change example to use signal

--- examples/timeout.rb
+++ examples/timeout.rb
@@ -4,4 +4,4 @@ require 'tty-command'
 
 cmd = TTY::Command.new
 
-cmd.run("while test 1; do echo 'hello'; sleep 1; done", timeout: 5)
+cmd.run("while test 1; do echo 'hello'; sleep 1; done", timeout: 5, signal: :KILL)
