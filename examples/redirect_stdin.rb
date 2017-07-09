42e2abac6abfaff4908d930ee1b2960794f5550a
Piotr Murach <pmurach@gmail.com>
2017-07-09 22:33:12 +0100

    Change example to use cli exec

--- examples/redirect_stdin.rb
+++ examples/redirect_stdin.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 
 require 'tty-command'
+require 'pathname'
 
+cli = Pathname.new('examples/cli.rb')
 cmd = TTY::Command.new
 
 stdin = StringIO.new
@@ -9,6 +11,6 @@ stdin.puts "hello"
 stdin.puts "world"
 stdin.rewind
 
-out, _ = cmd.run(:cat, :in => stdin)
+out, _ = cmd.run(cli, :in => stdin)
 
 puts "#{out}"
