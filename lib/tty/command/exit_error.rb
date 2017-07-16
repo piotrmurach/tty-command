56a8371510fe854604f69612c656c6b7ccc8d20f
Piotr Murach <pmurach@gmail.com>
2017-07-16 18:34:49 +0100

    Change to allow nil data for exit error

--- lib/tty/command/exit_error.rb
+++ lib/tty/command/exit_error.rb
@@ -20,8 +20,8 @@ module TTY
         message = ''
         message << "Running `#{cmd_name}` failed with\n"
         message << "  exit status: #{result.exit_status}\n"
-        message << "  stdout: #{result.out.strip.empty? ? 'Nothing written' : result.out.strip}\n"
-        message << "  stderr: #{result.err.strip.empty? ? 'Nothing written' : result.err.strip}\n"
+        message << "  stdout: #{(result.out || '').strip.empty? ? 'Nothing written' : result.out.strip}\n"
+        message << "  stderr: #{(result.err || '').strip.empty? ? 'Nothing written' : result.err.strip}\n"
       end
     end # ExitError
   end # Command
