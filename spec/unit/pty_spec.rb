5142ad4990ca615c1ddb04a051250cef2efd97aa
Piotr Murach <pmurach@gmail.com>
2018-04-22 15:57:48 +0100

    Change to output command

--- spec/unit/pty_spec.rb
+++ spec/unit/pty_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe TTY::Command, ':pty' do
   it "executes command in pseudo terminal mode as global option", unless: RSpec::Support::OS.windows? do
     color_cli = fixtures_path('color')
     output = StringIO.new
-    cmd = TTY::Command.new(output: output, pty: true)
+    cmd = TTY::Command.new(pty: true)#(output: output, pty: true)
 
     out, err = cmd.run(color_cli)
 
@@ -13,7 +13,7 @@ RSpec.describe TTY::Command, ':pty' do
   it "executes command in pseudo terminal mode as command option", unless: RSpec::Support::OS.windows? do
     color_cli = fixtures_path('color')
     output = StringIO.new
-    cmd = TTY::Command.new(output: output)
+    cmd = TTY::Command.new #(output: output)
 
     out, err = cmd.run(color_cli, pty: true)
 
