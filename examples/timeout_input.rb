64ea9e66573fb5e766dbdd21b8710e2f411f2e8b
Piotr Murach <pmurach@gmail.com>
2018-05-20 12:24:49 +0200

    Add example for infinite input timeout

---
+++ examples/timeout_input.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+require 'tty-command'
+
+cmd = TTY::Command.new
+
+path = File.expand_path("../spec/fixtures/infinite_input", __dir__)
+
+range = 1..Float::INFINITY
+infinite_input = range.lazy.map { |x| x }.first(10_000).join("\n")
+
+cmd.run(path, input: infinite_input, timeout: 2)
