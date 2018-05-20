56115ca240d0ca0852da2e328d411e5b84cd8bc3
Piotr Murach <pmurach@gmail.com>
2018-05-20 11:36:13 +0200

    Change to separate input tests

---
+++ spec/unit/input_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe TTY::Command, 'input' do
+  it "reads user input data" do
+    cli = fixtures_path('cli')
+    output = StringIO.new
+    command = TTY::Command.new(output: output)
+
+    out, _ = command.run("ruby #{cli}", input: "Piotr\n")
+
+    expect(out.chomp).to eq("Your name: Piotr")
+  end
+end
